c30daa546279e01e233166f42b15c5af3c6bd036
Tennessium <mighty.igor@yandex.ru>
2019-03-03 10:29:53 +0300

    Вот пока юзайте так, скоро допилю

--- main.cs
+++ main.cs
@@ -5,22 +5,16 @@ class Room {
   public int width = 0;
   public int height = 0;
   public int x = 0, y = 0;
-  public bool virgin = true;
   public bool is_horizontal = false;
   public int xr = 0, yr = 0, widthr = 0, heightr = 0;
   public int connected_to = 0;
+  public int connections = 1;
+  public bool connectable = true;
 
   public Room(int[] _width, int[] _height) {
     Random rnd = new Random();
     this.width = rnd.Next(_width[0], _width[1]);
     this.height = rnd.Next(_height[0], _height[1]);
-  }
-
-  public Room(Room room) {
-    this.width = room.width;
-    this.height = room.height;
-    this.x = room.x;
-    this.y = room.y;
   }
 
   public void set_coordinates(int[] coords) {
@@ -34,18 +28,8 @@ class Room {
     this.xr = rnd.Next(0, this.width - this.widthr);
     this.yr = rnd.Next(0, this.height - this.heightr);
   }
-
-  public void print() {
-    Console.Write("Room x = ");
-    Console.Write(this.x + this.xr);
-    Console.Write("\t room width is ");
-    Console.Write(this.widthr);
-    Console.Write("\t room y = ");
-    Console.Write(this.y + this.yr);
-    Console.Write("\t room height is ");
-    Console.WriteLine(this.heightr);
-  }
 }
+
 
 class Level {
   public int[][] map;
@@ -53,6 +37,7 @@ class Level {
   public double kmin = 0.3, kmax = 0.7;
   Room[] rooms;
   int rooms_count = 0;
+  private int path_size = 4;
 
   public Level(int[] _width, int[] _height) {
     Random rnd = new Random();
@@ -69,12 +54,26 @@ class Level {
     }
   }
 
+  public Level() {
+    this.width = 0;
+    this.height = 0;
+    this.resize(0);
+
+    this.map = new int[this.height][];
+    for (int i = 0; i < this.height; i++) {
+      this.map[i] = new int[this.width];
+      for (int j = 0; j < this.width; j++) {
+        this.map[i][j] = 0;
+      }
+    }
+  }
+
   private void resize(int k) {
     this.rooms = new Room[k];
   }
 
   public void print(string path) {
-    File.WriteAllText("data.txt", "");
+    File.WriteAllText(path, "");
     using (StreamWriter sw = File.AppendText(path)) {
       for (int i = 0; i < this.height; i++) {
         for (int j = 0; j < this.width; j++) {
@@ -113,13 +112,13 @@ class Level {
     }
   }
 
-
   public void split(Room room, Random rnd, int j) {
     int orientation = 1;
+    
     if (room.is_horizontal) {
       orientation = 0;
     } //0 - горизонтально 1 - вертикально
-    room.virgin = false;
+    
     if (orientation == 0) {
       int split_h = rnd.Next(Convert.ToInt32(room.height * this.kmin), Convert.ToInt32(room.height * this.kmax));
 
@@ -173,6 +172,54 @@ class Level {
     }
   }
 
+  private bool all_connected() {
+    int[][] arr = this.map;
+    bool ok = true;
+    int start_pos_x = 0, start_pos_y = 0;
+
+    for (int i = 0; i < arr.Length; i++) {
+      for (int j = 0; j < arr[i].Length; j++) {
+        if (arr[i][j] == 2) {
+          arr[i][j] = 1;
+          start_pos_x = j;
+          start_pos_y = i;
+        }
+      }
+    }
+
+    arr = this.ebanaya_recursiya(arr, start_pos_x, start_pos_y);
+
+    for (int i = 0; i < arr.Length; i++) {
+      for (int j = 0; j < arr[i].Length; j++) {
+        if (!(arr[i][j] == 0 || arr[i][j] == -1)) {
+          ok = false;
+        }
+      }
+    }
+
+    return ok;
+  }
+
+  private int[][] ebanaya_recursiya(int[][] _arr, int start_pos_x, int start_pos_y) {
+    int[][] arr = _arr;
+    if (!(arr[start_pos_y][start_pos_x] == 0 || arr[start_pos_y][start_pos_x] == -1)) {
+      arr[start_pos_y][start_pos_x] = -1;
+      try {
+        arr = this.ebanaya_recursiya(arr, start_pos_x + 1, start_pos_y);
+      } catch (System.Exception) {}
+      try {
+        arr = this.ebanaya_recursiya(arr, start_pos_x - 1, start_pos_y);
+      } catch (System.Exception) {}
+      try {
+        arr = this.ebanaya_recursiya(arr, start_pos_x, start_pos_y + 1);
+      } catch (System.Exception) {}
+      try {
+        arr = this.ebanaya_recursiya(arr, start_pos_x, start_pos_y - 1);
+      } catch (System.Exception) {}
+    }
+    return arr;
+  }
+
   private void connect(Room r1, Room r2, Random rnd) {
     if (r1.is_horizontal) { // если комнаты разделены вертикальной линией
       int min_h = r1.yr + r1.y;
@@ -184,12 +231,19 @@ class Level {
         max_h = r2.y + r2.yr + r2.heightr;
       }
 
-      for (int i = min_h; i < max_h; i++) {
+      int center_line = 0;
+      try {
+        center_line = rnd.Next(min_h + this.path_size / 2, max_h - this.path_size / 2);
+      } catch (System.Exception) {
+        center_line = (min_h + max_h) / 2;
+      }
+
+      for (int i = center_line - this.path_size / 2; i < center_line + this.path_size / 2; i++) {
         for (int j = r1.x + r1.xr + r1.widthr; j < r2.xr + r2.x; j++) {
           this.map[i][j] = 2;
         }
       }
-
+      
     } else {
       int min_w = r1.xr + r1.x;
       if (min_w < r2.x + r2.xr) {
@@ -200,7 +254,14 @@ class Level {
         max_w = r2.x + r2.xr + r2.widthr;
       }
 
-      for (int i = min_w; i < max_w; i++) {
+      int center_line = 0;
+      try {
+        center_line = rnd.Next(min_w+ this.path_size / 2, max_w - this.path_size / 2);
+      } catch (System.Exception) {
+        center_line = (min_w + max_w) / 2;
+      }
+
+      for (int i = center_line - this.path_size / 2; i < center_line + this.path_size / 2; i++) {
         for (int j = r1.y + r1.yr + r1.heightr; j < r2.y + r2.yr; j++) {
           this.map[j][i] = 2;
         }
@@ -210,6 +271,7 @@ class Level {
 
   public void generate(int dungeon_count, int min) {
     Random rnd = new Random();
+    this.path_size = min;
 
     int[][] size = new int[][] {
       new int[] {width, width},
@@ -243,19 +305,35 @@ class Level {
 
     // Вот эта херня в теории должна рисовать проходы между комнатами, но поа не рисует
     this.build_ends(rnd);
-    this.print("data.txt");
+    //this.print("data.txt");
+  }
+
+  
+  public int[][] generate(int width, int height, int deepth, int path_width) {
+    // Скрипт пишет поле в файл "data.txt"
+    int[][] size = new int[][] {
+      new int[] { width, width }, // Пределы для ширины
+      new int[] { height, height }, // Пределы для высоты
+    };
+    Level lvl = new Level(size[0], size[1]);
+    lvl.generate(deepth, path_width); // Глубина дерева или я хуй знает как это называется и ширина прохода
+    return lvl.map;
   }
 }
 
+
 class MainClass {
   public static void Main (string[] args) {
-    // Скрипт пишет поле в файл "data.txt"
-    int[][] size = new int[][] {
-      new int[] { 100, 100 }, // Пределы для ширины
-      new int[] { 100, 100 }, // Пределы для высоты
-    };
-    Level lvl = new Level(size[0], size[1]);
-    lvl.generate(5, 100); // Глубина дерева или я хуй знает как это называется и минимальные размеры комнаты (пока не работает)
-
+    Level lvl = new Level();
+    //Вот тебе и карта (последние 2 параметра не трогай пожалуйста, они пока только так работают :<)
+    int[][] kek = lvl.generate(10, 10, 2, 2);
+    
+    /*for (int i = 0; i < kek.Length; i++) {
+      for (int j = 0; j < kek[0].Length; j++) {
+        Console.Write(kek[i][j]);
+        Console.Write(" ");
+      }
+      Console.WriteLine(" ");
+    }*/
   }
 }
